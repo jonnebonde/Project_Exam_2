98a0ff82d446d64bc300671db72a92e4681b3ede
jonnebonde <jonnebonde@gmail.com>
2024-09-03 19:11:11 +0200

    made some changes to the rules of eslint and prettier

--- eslint.config.js
+++ eslint.config.js
@@ -27,13 +27,16 @@ export default [
     },
     rules: {
       "prettier/prettier": "error",
+      semi: ["error", "always"],
       ...js.configs.recommended.rules,
       ...react.configs.recommended.rules,
       ...react.configs["jsx-runtime"].rules,
       ...reactHooks.configs.recommended.rules,
-      ...eslintPluginPrettier.configs.recommended.rules,
       "react/jsx-no-target-blank": "off",
-      "react-refresh/only-export-components": ["warn", { allowConstantExport: true }],
+      "react-refresh/only-export-components": [
+        "warn",
+        { allowConstantExport: true },
+      ],
     },
   },
 ];
