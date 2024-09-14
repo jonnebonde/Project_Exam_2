5ccae4fefa27e7b7108a4445b37ecc29641118e6
jonnebonde <jonnebonde@gmail.com>
2024-09-15 00:16:03 +0200

    removed tailwind, added bootstrap

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from "react-router-dom";
 import Layout from "./Components/Layout";
 import Home from "./Pages/Home";
+import LoginPage from "./Pages/Login";
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
         <Route index element={<Home />} />
         <Route path="venue" element={"Venue"} />
         <Route path="Register" element={"Register"} />
-        <Route path="Login" element={"Login"} />
+        <Route path="Login" element={<LoginPage />} />
         <Route path="User" element={"User"} />
         <Route path="Venue_manager" element={"Venue manager"} />
       </Route>
--- src/Components/Layout/index.jsx
+++ src/Components/Layout/index.jsx
@@ -1,23 +1,34 @@
 import { Link, Outlet } from "react-router-dom";
 import Footer from "../Layout/Footer";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+
+// kan putte en if på om brukeren er logget inn eller ikke og som admin eller ikke.
+// Hvis man er logget inn, skal man ikke kunne logge inn og da forandre menyen deretter.
+// bruke Nav klassen som dynamisk meny etter hva som er logget inn eller ikke.
 
 function Header() {
   return (
-    <nav className="flex w-full justify-between bg-bg_cards mb-1 py-2 px-4 ">
-      <Link className="font-poppins font-bold text-2xl">Holidaze</Link>
-      <div className="flex gap-4 font-roboto font-medium items-center">
-        <Link>
-          <button className="border-2 rounded border-primary text-primary px-3.5 py-0 hover:underline focus:underline active:underline">
-            Login
-          </button>
+    <Navbar expand="sm" className="bg-body-tertiary">
+      <Container>
+        <Link className="navbar-brand fs-2" to="/">
+          Holidaze
         </Link>
-        <Link>
-          <button className="border-2 rounded border-primary bg-primary px-1 text-secondary_bg_color hover:underline focus:underline active:underline">
-            Register
-          </button>
-        </Link>
-      </div>
-    </nav>
+        <div className="col"></div>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+          <Nav className="">
+            <Link to="/Login" className="nav-link-login">
+              Login
+            </Link>
+            <Link to="/Register" className="nav-link-register">
+              Register
+            </Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
 
