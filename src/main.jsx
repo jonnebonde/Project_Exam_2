d05548a79adaf118e5595465ac617a1378348f29
jonnebonde <jonnebonde@gmail.com>
2024-09-16 17:59:58 +0200

    Revert "removed bootstrap"
    This reverts commit fd935069cc7ae62421fc68bf279d368a085d4f74.

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
+import "./sass/styles.scss";
 import { BrowserRouter } from "react-router-dom";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 
