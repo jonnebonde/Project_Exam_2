ec2b80782c59793250f115042ba4bee36edb9911
jonnebonde <jonnebonde@gmail.com>
2024-09-16 17:59:34 +0200

    Revert "innstalled tailwind"
    This reverts commit ec494971c2210968af400222e53f9bb242fce8fb.

--- src/Components/Layout/index.jsx
+++ src/Components/Layout/index.jsx
@@ -1,5 +1,8 @@
 import { Link, Outlet } from "react-router-dom";
 import Footer from "../Layout/Footer";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
 
 // kan putte en if på om brukeren er logget inn eller ikke og som admin eller ikke.
 // Hvis man er logget inn, skal man ikke kunne logge inn og da forandre menyen deretter.
@@ -7,21 +10,25 @@ import Footer from "../Layout/Footer";
 
 function Header() {
   return (
-    <header>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/home">Home</Link>
-          </li>
-          <li>
-            <Link to="/contact">Contact</Link>
-          </li>
-          <li>
-            <Link to="/login">Login</Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
+    <Navbar expand="sm" className="bg-body-tertiary">
+      <Container>
+        <Link className="navbar-brand fs-2" to="/">
+          Holidaze
+        </Link>
+        <div className="col"></div>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav" className="justify-content-end">
+          <Nav className="">
+            <Link to="/Login" className="nav-link-login">
+              Login
+            </Link>
+            <Link to="/Register" className="nav-link-register">
+              Register
+            </Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
   );
 }
 
