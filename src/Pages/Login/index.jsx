99aeb1b65a2b68b8dc1576e6d855328765ca0dc7
jonnebonde <jonnebonde@gmail.com>
2024-09-19 21:10:27 +0200

    minor work on login page

--- src/Pages/Login/index.jsx
+++ src/Pages/Login/index.jsx
@@ -2,8 +2,9 @@ import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
 import Container from "react-bootstrap/Container";
 import backgroundImage from "../../assets/Images/beach_with_sunset.jpg";
+import { Link } from "react-router-dom";
 
-//style form
+//style form og fikse container width på mindre skjerm størrelse
 
 function LoginPage() {
   return (
@@ -18,22 +19,26 @@ function LoginPage() {
         height: "100vh",
       }}
     >
-      <Form className="login-form bg-white p-5">
-        <h1 className="login-title mb-5 text-center">Login</h1>
+      <Form className="login-form bg-white p-4">
+        <h1 className="login-title mb-3 text-center">Login</h1>
         <Form.Group className="mb-3" controlId="formBasicEmail">
-          <Form.Label>Email address</Form.Label>
+          <Form.Label>Email</Form.Label>
           <Form.Control type="email" placeholder="Enter email" />
-          <Form.Text className="text-muted">
-            We share your emaisdfsdfsdffsl with anyone else.
-          </Form.Text>
+          <Form.Text className="text-muted"></Form.Text>
         </Form.Group>
         <Form.Group className="mb-3" controlId="formBasicPassword">
           <Form.Label>Password</Form.Label>
           <Form.Control type="password" placeholder="Password" />
         </Form.Group>
-        <Button variant="primary" type="submit">
-          Submit
+        <Button type="submit" className="w-100 mt-3 login-form-submit-button">
+          Login
         </Button>
+        <div className="text-center mt-5">
+          <p>Already have an account?</p>
+          <Link to="/register" className="nav-link-login">
+            Register
+          </Link>
+        </div>
       </Form>
     </Container>
   );
